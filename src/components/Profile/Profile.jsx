2a7cebeff9b30f94da67e3a371938c439fa7da5e
Valentina Perekhrest <valentina.perehrest.01@gmail.com>
2022-12-05 00:28:07 +0200

    add profile

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,73 @@
+
+const Profile = ({avatar, username, tag, location, stats}) => {
+   return (
+    <div class="profile">
+    <div class="description">
+      <img
+        src={avatar}
+        alt="User avatar"
+        class="avatar"
+      />
+      <p class="name">{username}</p>
+      <p class="tag">@{tag}</p>
+      <p class="location">{location}</p>
+    </div>
+  
+    <ul class="stats">
+      <li>
+        <span class="label">Followers</span>
+        <span class="quantity">{stats.followers}</span>
+      </li>
+      <li>
+        <span class="label">Views</span>
+        <span class="quantity">{stats.views}</span>
+      </li>
+      <li>
+        <span class="label">Likes</span>
+        <span class="quantity">{stats.likes}</span>
+      </li>
+    </ul>
+  </div>
+    );
+
+};
+
+export default Profile;
+
+// --------------------------------
+
+// export default function Profile ({avatar, userName, tag, location, stats) {
+//     return (
+// //     <div class="profile">
+//     <div class="description">
+//     <img
+//       src={avatar}
+//       alt="User avatar"
+//       class="avatar"
+//     />
+//     <p class="name">{username}</p>
+//     <p class="tag">@{tag}</p>
+//     <p class="location">{location}</p>
+//   </div>
+
+//   <ul class="stats">
+//     <li>
+//       <span class="label">Followers</span>
+//       <span class="quantity">{stats.followers}</span>
+//     </li>
+//     <li>
+//       <span class="label">Views</span>
+//       <span class="quantity">{stats.views}</span>
+//     </li>
+//     <li>
+//       <span class="label">Likes</span>
+//       <span class="quantity">{stats.likes}</span>
+//     </li>
+//   </ul>
+// </div>
+//     );
+ 
+//  };
+
+
+

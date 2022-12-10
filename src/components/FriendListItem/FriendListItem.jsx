a156354d1f1bf025ce613d8e21dd74e1f96d7fbe
Valentina Perekhrest <valentina.perehrest.01@gmail.com>
2022-12-10 14:33:32 +0200

    add

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
 import css from './FriendListItem.module.css';
 
-const FriendListItem = ({avatar, name, isOnline})=>{
-    return ( <li className={css.item}>
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={css.item}>
       <span className={isOnline ? css.online : css.offline}></span>
-     <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-     <p className={css.name}>{name}</p>
-   </li>);
-   
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
 };
 
 FriendListItem.propTypes = {

e1865aef3ac26e0fec4affe25e85863569f06cfb
Valentina Perekhrest <valentina.perehrest.01@gmail.com>
2022-12-10 16:44:04 +0200

    add

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,28 +5,30 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
- const Statistics = ({stats, title})=>{
-    return (
-       <section className={css.statistics}>
-    {title && <h2 className={css.title}>{title}</h2>}
-  
-    <ul className={css.stat__list}>
-   {stats.map(({id, label, percentage})=>(
-         <li key = {id}
-        className= {css.item}
-         style={{ 
-          backgroundColor: getRandomHexColor(),}}> 
-         <span className={css.label}>{label}</span>
-         <span className={css.percentage}>{percentage}%</span>
-      </li>
-      ))}
-    </ul>
-  </section>   
-      );
-   
- };
+const Statistics = ({ stats, title }) => {
+  return (
+    <section className={css.statistics}>
+      {title && <h2 className={css.title}>{title}</h2>}
 
- Statistics.propTypes = {
+      <ul className={css.stat__list}>
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            key={id}
+            className={css.item}
+            style={{
+              backgroundColor: getRandomHexColor(),
+            }}
+          >
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+Statistics.propTypes = {
   title: PropTypes.string,
   stats: PropTypes.arrayOf(
     PropTypes.shape({
